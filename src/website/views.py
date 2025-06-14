c55e26c6b6584836d619c3a998b38d26bdf96723
Nelson Wells <nelsonwellswku@gmail.com>
2025-06-13 21:38:03 -0500

    Add list of quizzes to take to home page;
    give a message to the user once they've completed a quiz

--- src/website/views.py
+++ src/website/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.http import Http404
 from django.shortcuts import render
 from django.views.decorators.http import require_GET, require_POST
@@ -6,7 +7,8 @@ from website.forms import QuizForm
 
 
 def home(request):
-    return render(request, "home.html")
+    quizzes = Quiz.objects.all()[:10]
+    return render(request, "home.html", {"quizzes": quizzes})
 
 
 @require_GET
@@ -36,16 +38,20 @@ def post_quiz_answer(request, slug):
 
     # todo: what to actually do when the submitted form is invalid?
     if not quiz_form.is_valid():
-        raise Http404("Not valid")
+        raise ValidationError("Form is invalid")
 
     try:
         requested_quiz = Quiz.objects.get(pk=quiz_form.cleaned_data["quiz_id"])
     except Quiz.DoesNotExist:
         raise Http404("Quiz does not exist.")
 
-    question = requested_quiz.question_set.filter(
-        question_id__gt=quiz_form.cleaned_data["question_id"]
-    ).order_by("question_id")[0]
+    try:
+        question = requested_quiz.question_set.filter(
+            question_id__gt=quiz_form.cleaned_data["question_id"]
+        ).order_by("question_id")[0]
+    except IndexError:
+        return render(request, "quiz_card_finished.html")
+
     answers = question.answer_set.all()
 
     return render(
@@ -53,4 +59,3 @@ def post_quiz_answer(request, slug):
         "quiz_card.html",
         {"quiz": requested_quiz, "question": question, "answers": answers},
     )
-
